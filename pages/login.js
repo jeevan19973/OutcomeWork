a309f5c26e34516e58e8524836cbe2eecb860912
jeevan19973 <suryamaddu@yahoo.in>
2024-11-11 18:42:43 -0500

    Adding files via API - 2024-11-11 23:42:40

--- pages/login.js
+++ pages/login.js
@@ -1,12 +1,10 @@
 import { signIn } from 'next-auth/react';
-import React from 'react';
-import Button from '@mui/material/Button';
 
 const Login = () => {
   return (
     <div>
       <h1>Login</h1>
-      <Button variant="contained" onClick={() => signIn('google')}>Log in with Google</Button>
+      <button onClick={() => signIn('google')}>Log in with Google</button>
     </div>
   );
 };
