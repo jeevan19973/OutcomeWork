8b964b72407520b442095dda1ab3f26f51c64335
jeevan19973 <suryamaddu@yahoo.in>
2024-11-11 18:28:14 -0500

    Adding files via API - 2024-11-11 23:28:10

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,26 @@
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      session.user.role = token.role;
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.role = user.role;
+      }
+      return token;
+    }
+  },
+  pages: {
+    signIn: '/login',
+  }
+});
