bb199f8afded040622720c7b9d7f173ea166b04e
jeevan19973 <suryamaddu@yahoo.in>
2024-11-11 18:28:12 -0500

    Adding files via API - 2024-11-11 23:28:10

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to MyApp</h1>
+      <p>Login to continue.</p>
+    </div>
+  );
+};
+
+export default Home;
