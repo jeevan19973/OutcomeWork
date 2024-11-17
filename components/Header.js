54649576c26081aa32676087e3767b2c46d25253
jeevan19973 <suryamaddu@yahoo.in>
2024-11-17 13:37:55 -0500

    Adding files via API - 2024-11-17 18:37:49

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,7 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography } from '@mui/material';
 
 const Header = () => {
-  return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6">My App</Typography>
-      </Toolbar>
-    </AppBar>
-  );
+  return <header><h1>App Header</h1></header>;
 };
 
 export default Header;
